ec5c741aaf74b82aed020ea03f351d78bbc04fee
David Strang <davidryanstrang@gmail.com>
2021-06-16 18:14:06 -0700

    Update dependencies. Update <Overlay>

--- frontend/src/components/Overlay.vue
+++ frontend/src/components/Overlay.vue
@@ -1,26 +1,33 @@
 <template>
     <teleport to="body">
-        <div v-if="show" class="overlay">
+        <div
+            v-if="show"
+            class="fixed top-0 left-0 w-full h-full"
+            :class="background"
+        >
             <slot></slot>
         </div>
     </teleport>
 </template>
 
 <script>
+import { computed } from 'vue'
+
 export default {
     props: {
         show: Boolean,
+        transparent: Boolean,
+    },
+    setup(props) {
+        const background = computed(() => {
+            if (!props.transparent) {
+                return ['bg-black', 'bg-opacity-30']
+            }
+        })
+
+        return {
+            background,
+        }
     },
 }
 </script>
-
-<style>
-.overlay {
-    position: fixed;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    background-color: rgba(0, 0, 0, 0.3);
-}
-</style>
