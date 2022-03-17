715b3adca054d3dae2a1d4bfe6e4e94facb0ce7e
Jesse Viola <jesse.viola@gmail.com>
2022-03-16 21:52:56 -0700

    Removing GOLANG, adding Python/Poetry

--- backend/pkg/login/store.go
+++
@@ -1,5 +0,0 @@
-package login
-
-// TODO Login strute will be the entity of the data.
-type Login struct {
-}
--- backend/scripts/migrate.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Enable golang to be used in this script
-GO111MODULE=on go install -tags 'postgres' github.com/golang-migrate/migrate/v4/cmd/migrate@latest
-
-# TODO make a check to the PG DB metadata versions table and check if we need to migrate
-# 
