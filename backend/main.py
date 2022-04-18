b50bd70094eb64ef5646c2942ac5697876b070ec
Jesse Viola <jesse.viola@gmail.com>
2022-04-17 22:27:56 -0700

    fixing mac docker problems with local

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,4 @@
 from fastapi import FastAPI
-import uvicorn
 
 from server.router import api_router as v1_router
 
@@ -8,5 +7,5 @@ app = FastAPI()
 app.include_router(v1_router)
 
 # For debugging
-if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+# if __name__ == "__main__":
+#     uvicorn.run(app=app, host="0.0.0.0", port=8000)
--- backend/server/scrape/routes.py
+++ backend/server/scrape/routes.py
@@ -1,3 +1,4 @@
+from typing import Optional
 from fastapi import APIRouter
 
 from server.managers import scrape_manager
@@ -7,5 +8,13 @@ router = APIRouter()
 
 
 @router.post("/recipe", response_model=Recipe)
-async def get_recipe(url_in: URL) -> Recipe:
+async def get_recipe(url_in: URL) -> Optional[Recipe]:
+    """Returns a recipe from the given URL
+
+    Args:
+        url_in (`schemas.URL`): model URL
+
+    Returns:
+        Recipe: Represents a recipe from the given URL in string format.
+    """
     return scrape_manager.get_recipe(url_in.link)
